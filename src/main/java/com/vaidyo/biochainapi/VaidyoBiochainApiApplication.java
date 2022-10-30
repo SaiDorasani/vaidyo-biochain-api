9849858fb1e3eb685822647e31adeec79ff84f07
SaiDorasani <87720670+SaiDorasani@users.noreply.github.com>
2022-10-29 21:16:22 -0500

    Initial Vaidyo Biochain api structure
    Commit the skeleton structure for vaidyo biochain api

---
+++ src/main/java/com/vaidyo/biochainapi/VaidyoBiochainApiApplication.java
@@ -0,0 +1,13 @@
+package com.vaidyo.biochainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaidyoBiochainApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaidyoBiochainApiApplication.class, args);
+    }
+
+}
